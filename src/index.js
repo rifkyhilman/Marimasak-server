1384245d7bf8e539960be794f065cedaa0ba18d6
rifkyhilman <rifkyhilman265@gmail.com>
2025-01-14 15:02:26 +0700

    :memo: Add connection to database
    add connection to database

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const connectDB = require("./database/db");
+const port = process.env.PORT;
+
 const app = express();
 
-dotenv.config();
-
-const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
 app.get("/", (req, res) => {
     res.send("Hello World !!");
 });
 
-app.listen(PORT, () => {
-    console.log("Express API running in port:" + PORT);
+connectDB();
+
+app.listen(port, () => {
+    console.log(`Express API running in port: ${port}`);
 });
